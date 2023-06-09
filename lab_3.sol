d00f2332088b2cde2d62d10f8398fe588bc0ce82
candyflipprog <122864410+candyflipprog@users.noreply.github.com>
2023-06-09 09:56:02 +0300

    finally fix

--- lab_3.sol
+++ lab_3.sol
@@ -25,11 +25,12 @@ contract Voting {
 
     uint public numHeadmans = 0;
     uint public numStudents = 0;
+    uint public timeVoted = 0;
 
     address admin = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
-    uint VotedTime = 60;
     bool VotedStatus = false;
+    bool public votingStarted = false;
 
     constructor() {
         studentsArr[numStudents] = Students(numStudents, "Ivanov", "ISiP(p) 2/3", 1, false);
@@ -43,6 +44,8 @@ contract Voting {
 
         studentsArr[numStudents] = Students(numStudents, "Semenov", "KSK 2/27", 1, false);
         numStudents++;
+
+        timeVoted = block.timestamp + 1 minutes; 
     }
 
     function addNewStudent(uint _ID, string memory _Surname, string memory _Group) public {
@@ -63,17 +66,36 @@ contract Voting {
         numHeadmans++;
     }
 
-    function voted(uint _ID, string memory _Group) public {
-        require(block.timestamp < VotedTime, "Voted is not started");
-        for(uint i = 0; i < numStudents; i++) {
-            require(studentsArr[i].statusVoted, "You are already voted!");
-        }
-        for(uint i = 0; i < numStudents; i++) {
-            require(keccak256(abi.encodePacked(studentsArr[i].group)) == keccak256(abi.encodePacked(_Group)), "You are not this group!");
-            for(uint j = 0; j < numHeadmans; j++) {
-                require(keccak256(abi.encodePacked(headmansArr[i].group)) == keccak256(abi.encodePacked(_Group)), "You are not this group");
+    function startVoted() public {
+        require(msg.sender == admin, "You are not admin!");
+        require(!votingStarted, "Voting has already started!");
+        votingStarted = true;
+    }
+
+    function vote(uint _studentID, uint _headmanID) public {
+        require(votingStarted, "Voting has not started yet");
+        require(block.timestamp <= timeVoted, "Voting period has ended");
+        require(!studentsArr[_studentID].statusVoted, "You have already voted");
+        require(keccak256(abi.encodePacked(studentsArr[_studentID].group)) == keccak256(abi.encodePacked(headmansArr[_headmanID].group)), "You are not in the same group");
+
+        studentsArr[_studentID].statusVoted = true;
+        headmansArr[_headmanID].votedNumber++;
+    }
+
+    function getVotingResult() public view returns (string memory) {
+        require(votingStarted, "Voting has not started yet");
+        require(block.timestamp >= timeVoted, "Voting is still ongoing");
+
+        uint maxVotes = 0;
+        uint winnerIndex;
+
+        for (uint i = 0; i < numHeadmans; i++) {
+            if (headmansArr[i].votedNumber > maxVotes) {
+                maxVotes = headmansArr[i].votedNumber;
+                winnerIndex = i;
             }
         }
-        
+
+        return string(abi.encodePacked("The winner is ", headmansArr[winnerIndex].surname));
     }
 }
